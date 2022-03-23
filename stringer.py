7da32657e34e144182ba16a32585e75de1b533c8
twono <twono.dev@gmail.com>
2022-03-23 00:39:35 +0000

    Add usage message

--- stringer.py
+++ stringer.py
@@ -1,7 +1,15 @@
 import sys
 
+def print_usage():
+    print("Usage:")
+    print("stringer.py 1 <file> <string_to_count>")
+    print("            2 <file> <string_to_remove>")
+
 if __name__ == "__main__":
-    print(f"There are {len(sys.argv)} arguments and the first one is {sys.argv[0]}")
+    if len(sys.argv) < 2 or sys.argv[1] == "-h":
+        print_usage()
+        exit(1)
+
     with open(sys.argv[1]) as input:
         for line in input:
             print(f"{line.strip()}")
